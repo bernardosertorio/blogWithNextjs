96dae80884c466bd76691b26d86e05457cd8f783
Bernardo Sertório <besertorio@gmail.com>
2021-05-07 19:14:51 -0300

    using Link component in header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,21 @@
+import Link from 'next/link';
 import styles from './header.module.scss';
 
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <a href="/">
-          <img className={styles.vector} src="/images/Vector.svg" alt="logo" />
-          <img className={styles.logo} src="/images/logo.svg" alt="logo" />
-          <img className={styles.svg} src="/images/svg.svg" alt="logo" />
-        </a>
+        <Link href="/" prefetch>
+          <a>
+            <img
+              className={styles.vector}
+              src="/images/Vector.svg"
+              alt="logo"
+            />
+            <img className={styles.logo} src="/images/logo.svg" alt="logo" />
+            <img className={styles.svg} src="/images/svg.svg" alt="logo" />
+          </a>
+        </Link>
       </div>
     </header>
   );
